eab0c45ebed76b02d86166dde21d30663384ce84
黄千祐 <oncwnuDd3F-v4h9pVDHDkkvq6Xl0@git.weixin.qq.com>
2019-09-03 00:10:01 +0800

    移动

--- src/main/java/com/qianyou/www/config/Swagger2.java
+++ src/main/java/com/qianyou/www/config/Swagger2.java
@@ -1,4 +1,4 @@
-package com.qianyou.www;
+package com.qianyou.www.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/qianyou/www/controller/QuestionWechatController.java
+++ src/main/java/com/qianyou/www/controller/QuestionWechatController.java
@@ -1,4 +1,4 @@
-package com.qianyou.www;
+package com.qianyou.www.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
